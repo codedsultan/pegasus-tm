d7d69e96d85ba74e2103cf01a3300ef12fd5d045
codesultan <codesultan369@gmail.com>
2025-01-05 19:51:27 +0100

    x-migrations

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { createInertiaApp } from '@inertiajs/react';
 // import { createRoot } from 'react-dom';
 import { createRoot, hydrateRoot } from 'react-dom/client';
-import { resolvePageComponentForReact } from './helpers/inertia-helpers';
+import { resolvePageComponentForReact } from './Helpers/inertia-helpers';
 import { Provider } from "react-redux";
 import { store } from './redux/store.js'
 import '../css/app.scss';
